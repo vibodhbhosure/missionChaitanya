bac4af6753cd18fdbaffafe9df39299a593f89c9
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-10-05 14:40:03 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Navbar() {
+    return(
+    <nav className="navbar">
+        <h1 className="heading">Mission Chaitanya</h1>
+    </nav>
+    )
+}
