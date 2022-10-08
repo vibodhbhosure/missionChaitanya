326c7b739a90d7c826a395dda7e6c090028edaf3
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-10-08 14:06:24 +0530

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,38 @@
 import React from "react";
 export default function Navbar() {
-    return(
-    <nav className="navbar">
-        <h1 className="heading">Mission Chaitanya</h1>
-    </nav>
+    return (
+        <nav className="navBar" >
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+                <a className="navbar-brand" href="#">
+                    <img src="https://toppng.com/uploads/preview/om-symbol-transparent-background-11549837690tn2ios9vux.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info <span class="sr-only">(current)</span></a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Why we do</a>
+                                <a class="dropdown-item" href="#">Conflicts Resolution</a>
+                            </div>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Donate</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About Us</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link " href="#">Contact Us</a>
+                        </li>
+<div>
+                        <button type="button" id="register_login" className="btn btn-dark">Register/Login</button></div>
+                    </ul>
+                </div>
+            </nav>
+        </nav>
     )
 }
