470de0ddfc404f6b36d3bf758f230d0ea25ae741
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-10-12 01:41:44 +0530

    Added some page in why do we and made card in conflict and confusion

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,18 +7,18 @@ import { Link } from "react-router-dom";
 
 export default function Card(props) {
     return (
-     <div>
+     <div className="main">
      
      <div className="card cards" >
   <img className="card-img-top" id="cardimag" src={props.src} alt="cardimage"/>
-  <div className="card-body">
-    <h5 className="card-title title">{props.title}</h5>
+  <div className="card-body cardBody">
+    <h5 className="card-title title-card">{props.title}</h5>
     <p className="card-text desc">{props.desc}</p>
   </div>
   
-  <div className="card-body">
- <Link to={props.link}> <button type="button" className="btn btn-info">View More</button></Link>
-  </div>
+ 
+ <Link to={props.link}> <button type="button" className="btn btn-info btncard">View More</button></Link>
+  
 </div>
      </div>
     
---
+++ src/components/ConflictsandConfusion/CasteSystem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function CasteSystem() {
+    return (
+        <div >
+  
+       ring bell
+        </div>
+        
+    )
+}
