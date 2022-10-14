0ac23f593ee2ec794f4e388d5d53fe6ace1d56c9
SnehalSLohar <snehallohar17@gmail.com>
2022-10-14 08:20:10 +0530

    death and life page

--- src/components/ConflictsandConfusion/LifeDeath.js
+++ src/components/ConflictsandConfusion/LifeDeath.js
@@ -1,12 +1,160 @@
 import React from "react";
 
-
 export default function IdolWorship() {
-    return (
-        <div >
-  
-       lIFE DEATH
+  return (
+    <div className="main">
+      <h2 className="title">LIFE, DEATH AND REBIRTH</h2>
+      <div className="container">
+        <div>
+          <img
+            className="img"
+            src="https://static.toiimg.com/photo/msid-94565189.cms"
+          />
         </div>
-        
-    )
+        <div>
+          <p>
+            What is life? What is birth and death? Why does one suffer while
+            another experiences joy? Is this the only birth? These are some of
+            the fundamental questions that every thinking individual seeks an
+            answer to. There is much confusion, scepticism or disbelief even
+            amongst practising Hindus.
+            <br />
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <p>
+            <u>
+              <b>Two bodies</b>
+            </u>
+            <br />
+            Every individual has a gross body which is perceived and a subtle
+            body which cannot be perceived. The gross body is made up of the
+            five gross elements (space, air, fire, water and earth), is born
+            from the mother's womb and is the counter of all experiences of
+            life. It grows, decays, and finally perishes at death. It is buried
+            or burnt by others and returns to the elements of which it is
+            composed.The subtle body is made up of the five subtle elements, It
+            consists of the five sense organs (ability to hear, feel, see, taste
+            and smell), the five organs of action (ability to speak, grasp,
+            move, excrete and procreate), the five physiological functions
+            (breathing, excretion, circulation, ejection and digestion) and
+            four-fold inner equipment (mind, intellect, memory and the I-notion
+            or ego).
+          </p>
+
+          <p>
+            <u>
+              <b>The self</b>
+            </u>
+            <br />
+            Within, yet beyond the two bodies, is the pure, infinite Self (Atma)
+            which is neither born nor dies, It is also called Truth (Satyam),
+            Reality (Brahman) or Consciousness (Caitanya) in philosophy and God
+            (Isvara) in religion.
+          </p>
+        </div>
+        <div>
+          <img
+            className="img"
+            src="https://images.languagehumanities.org/caste-system-in-india.jpg"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <img
+            className="img"
+            src="https://c.files.bbci.co.uk/C7B5/production/_88452115_brahmaalamy976.gif"
+          />
+        </div>
+        <div>
+          <p>
+            <u>
+              <b>What is life?</b>
+            </u>
+            <br />
+            Life (jīvanam) is the manifestation of Consciousness /Self in the
+            gross body through the medium of the subtle body. Let us take the
+            analogy of a light bulb. The light (life) is the manifestation of
+            electricity (Self) in the bulb (gross body) through the medium of
+            the filament (subtle body). Whenever there is light (life) there
+            must be electricity (Consciousness/Self). But wherever there is
+            electricity (Consciousness/Self) there need not be light (life). The
+            stone has Consciousness/Self/ God but no life, since it does not
+            have a subtle body which alone can manifest life.
+            <br />A being with a more developed subtle body is considered higher
+            in the ladder of evolution. The amoeba is thus amongst the lowest of
+            life forms and the human being amongst the highest.
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <p>
+            <u>
+              <b>What is born and what dies?</b>
+            </u>
+            <br />
+            The body is ever changing and perishing. The Consciousness/Self is
+            unchanging and immortal. Connecting these two is the sentient ego,
+            the individual which borrows its sentiency from the Self and
+            identifies with the name, form and qualities of the two bodies. It
+            owns the two bodies (I am fat, I am glad...), their actions (I sing,
+            I think...) and the results reaped thereof (I enjoy, I suffer...).
+            It goes through the three states of experience (I am awake, I dream
+            and I sleep). It is this individuality alone that goes through the
+            experience of birth and death (today is my birthday, I shall die one
+            day...).
+          </p>
+        </div>
+        <img
+          className="img"
+          src="https://i0.wp.com/lawyersgyan.com/wp-content/uploads/2018/05/varna-system-caste-system-in-india.jpg?fit=960%2C540&ssl=1"
+        />
+      </div>
+
+      <div className="container">
+        <div>
+          <img
+            className="img"
+            src="https://c.files.bbci.co.uk/C7B5/production/_88452115_brahmaalamy976.gif"
+          />
+        </div>
+        <div>
+          <p>
+            <u>
+              <b>What is birth and death?</b>
+            </u>
+            <br />
+            It is commonly accepted that the emergence of the child from the
+            womb is its birth and that the ebbing away of life represents the
+            death of a person. However in the light of what has been said
+            earlier, birth is the entry of the subtle body into the gross body
+            along with the individuality (jiva) that owns both. Some say that
+            the individual enters at conception and some that it enters a few
+            months later. <br />
+            However it is commonly believed that the foetus in the womb is
+            alive. Death is the departure of the individual from the gross body.
+            The presence of the subtle body alone is the indicator of life in
+            the body.
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <p>
+          <b>
+            <i>
+              In conclusion, Hindus are not idol worshippers. The idol
+              represents the ideal (God) to be worshipped. Thus the infinite and
+              all-pervading God can also be worshipped in the idol with
+              offerings.
+            </i>
+          </b>
+        </p>
+      </div>
+      <div className="footer">&copy; Mission Chaitanya</div>
+    </div>
+  );
 }
