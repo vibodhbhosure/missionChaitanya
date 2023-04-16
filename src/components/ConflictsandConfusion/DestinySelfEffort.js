6f4679700e6d7bdafa53d7f488e5ec0ebc6041dc
Mrunal Tamhane <mrunal.tamhane7@gmail.com>
2023-04-16 22:04:48 +0530

    added missing content

--- src/components/ConflictsandConfusion/DestinySelfEffort.js
+++ src/components/ConflictsandConfusion/DestinySelfEffort.js
@@ -3,126 +3,88 @@ import React from "react";
 export default function destinySelfEffort() {
   return (
     <div className="main">
-      <h2 className="title">IDOL WORSHIP</h2>
+      <h2 className="title">DESTINY AND SELF EFFORT</h2>
       <div className="container">
         <div>
           <img
             className="img"
-            src="https://static.toiimg.com/photo/msid-94565189.cms"
+            src="https://i.pinimg.com/564x/e0/20/2a/e0202a53c6a1e1ac14a5fe279d18626a.jpg"
           />
         </div>
         <div>
           <p>
-            Wherever there are human beings - in India as well as all over the
-            world-there exists the 'varna vyavastha". Mistakenly called the
-            'caste system, it has created misunderstandings and negative
-            connotations. Here, we will try and understand this universal system
-            in more detail and with an open mind.
+            <b>What is fate? Are some destined to fail despite their best efforts? Are some just plain lucky? Is God partial? Can we change our destiny? Are Hindus fatalistic?
+
+            What is destiny and self effort?</b><br/>
+
+            Every action must give its result. Cause precedes effect. The action done in the present will always bear result in the future - be it near or distant. Therefore what we get in the present is always due to our recent or distant past actions. The result of action itself is variously called - karma phala, prărabdha, fate, destiny or luck. The action performed by the individual is called self effort or puruşärtha.
+
           </p>
           <p>
-            The word 'varna' means to describe. One describes an object by its
-            qualities. Varna also means colour, hence the literal translation of
-            varna vyavastha is Colour System. Colour obviously does not mean the
-            colour of the skin. Human beings are categorized into four types
-            (varnas) on the basis of their qualities (gunas) and professions
-            (karmas). This fourfold categorisation in India is called the Varna
-            System.
+          <b>Can we change our destiny?</b><br/>
+
+            Swami Chinmayananda says, <i>'What we get in life is our destiny. What we do with what we get is our self effort.'</i> In the light of this, it is we who create our own destiny which we are fated to enjoy or suffer. Even though we have no freedom in what we get, we can choose what to do with what we get.
+
+            Having once done an action, we cannot undo it. Every effort at undoing is a new action which will produce its own result. One action does not cancel the result of another. If a person commits ten good acts and ten bad acts, he will reap the results of twenty acts. Even though the good act does not cancel the effect of the bad act, it helps to create a better future or destiny for the person. The good acts also give the strength needed to go through the results of the bad actions.
+
           </p>
         </div>
       </div>
       <div className="container">
         <div>
           <p>
-            <b>The Four Types of Human Beings</b>
-            <br />
-            <br />
-            The fourfold categorisation of human beings on the basis of
-            qualities (gunas) is as below:
-            <br />
-            1. Brahmin: Predominantly sattva (approx. 70%) backed by rajas
-            (approx. 20%) and tamas (approx.10%).
-            <br /> 2. Ksatriya: Predominantly rajas (approx. 70%)backed by
-            sattva (approx. 20%) and tamas (approx. 10%). <br />
-            3. Vaisya: Predominantly rajas (approx. 70%) backed by tamas
-            (approx. 20%) and sattva (approx.10%). <br />
-            4. Südra: Predominantly tamas (approx. 70%) backed by rajas (approx.
-            20%) and sattva (approx.10%).
+          <b>Will the past always weigh on us?</b><br/>
+
+            Before starting any task, we weigh all the pros and cons and assess our assets and liabilities. Before embarking on a journey we understand the challenges that we will have to face from the beginning to the end. Similarly, in life's journey we should face our negative prarabdha as challenges and the positive ones as blessings.
+
           </p>
 
           <p>
-            It should, of course, be understood that the proportions are
-            approximate and not to be taken literally. They vary in different
-            people but what is noteworthy is that all of us have each of the
-            three qualities. No human being, however good or bad, can claim to
-            have only one or two of the three. Also the proportion of these
-            gunas may vary in us at different times and stages in life. It is
-            possible that a lethargic and laid-back person may, at a time in
-            life, become dynamic and a go-getter. Ratnakara, a dacoit did become
-            the famous composer of the Rāmāyaṇa, Sage Valmiki. Such drastic
-            changes are however not frequent.
+
+            Role of God in karma
+
+            If I get results according to what I do, what is the role of God? The body by itself is inert matter incapable of any action. It is Consciousness/Self/God that enlivens it and empowers it to act. God also creates the world - the field - in which man sows the seeds of action and reaps the results. Both the actions and the results are created and governed by God made laws. He is therefore karma-adhyaksa (one who presides over actions) and karma-phaladātā (giver of results of actions). Nature's/God's laws are neither partial nor prejudiced. They appear just and kind to some, and partial and harsh to others. The teacher gives marks according to what we have written in the test. The teacher also gives grace marks to the deserving, who, but for the 'grace' might have failed the test!
+
           </p>
         </div>
         <div>
           <img
             className="img"
-            src="https://images.languagehumanities.org/caste-system-in-india.jpg"
+            src="https://i.pinimg.com/564x/11/c0/38/11c0383f403288c662c31990b961bf04.jpg"
           />
         </div>
       </div>
       <div className="container">
-        <div>
-          <img
-            className="img"
-            src="https://c.files.bbci.co.uk/C7B5/production/_88452115_brahmaalamy976.gif"
-          />
-        </div>
+        
         <div>
           <p>
-            <b>Four Types of Professions</b>
-            <br />
-            <br />
-            The three qualities prompt the four types of human beings into
-            different professions:
-            <br />
-            1. Brahmins: Being knowledge and peace oriented, they become
-            teachers, researchers, priests, advisors, visionaries, artists and
-            so on. <br />
-            2. Ksatriya: Since they are action oriented and backed by noble
-            intentions they become rulers, leaders, social workers, patrons of
-            fine arts and so on.
-            <br /> 3. Vaisya: Even though they are action oriented like the
-            kñatriyas, they are backed by profit motive and with an ability to
-            mobilise and create wealth and resources. They, therefore, become
-            traders, farmers, businessmen, entrepreneurs, moneylenders and so
-            on. <br />
-            4. Sudras: Not being inclined towards knowledge or action, they are
-            not self-motivated. They become good labourers, helpers, artisans,
-            craftsmen, farmhands and so on.
+           <b> Are Hindus fatalistic?</b><br/>
+
+            Many blame the laws of karma for their fatalistic attitude. Some await the turn of destiny to progress and prosper. Some blame the star signs for their problems. Some accept abject situations as their lot and believe that they are doomed to fail or to remain mediocre.
+
+            In contrast, the scriptures and saints urge man to work hard and prosper. obstacles in achieving one's desired goal. Hands to face and overcome all symbolise self effort. Each morning the Hindu looks at his hands and prays, 'Wealth, Knowledge and Divinity reside in my hand. May I attain them through righteous efforts. It is also said, 'My hands are indeed fortunate- as I can please Him with my sincere efforts. (ayam me hasto bhagavan)
+
+
           </p>
         </div>
       </div>
       <div className="container">
         <div>
           <p>
-            When people take to professions (karmas) according to their
-            qualities (gunas) it is like fish taking to water. They have natural
-            inclination towards their jobs and so love what they do. Hence, they
-            grow, prosper and become happy. If however, the gunas and karmas do
-            not complement each other, there is a lot of hypocrisy, tension and
-            frustration within, confusion and chaos in the field of action and
-            suffering for all.
+
+            The right attitude in facing destiny and self effort
+
+            We are generally tense and anxious whilst doing actions and clated or dissatisfied, frustrated or dejected by the results. This takes away the joy in action. The attitude of Karma Yoga enables one to consistently experience joy in action. We should selflessly perform all actions dedicated to a noble cause or to God (Isvara arpana buddhi) and cheerfully receive all results - good or bad, as gifts from Him (prasada buddhi). To quote Swami Chinmayananda,<i>"What we have is His gift to us. What we do with what we have is our gift to Him."</i> 
+            Such an attitude keeps man motivated and inspired in action, yet fulfilled and content with the results.
+
+            In conclusion, what we get as a result of our actions is our destiny and what we do with what we get is our self effort. Destiny is thus the challenge we face in life's journey. The right attitude in doing actions and facing results makes this journey enjoyable and fulfilling. Therefore we should do our best and leave the rest to Him, whose just and kind laws actions and results.
+
           </p>
-          <p>
-            A businessman who lawfully makes huge profitsis admired, respected
-            and awarded. On the other hand, a politician who works only for huge
-            profits is condemned and disrespected. Mahatma Gandhi had a mass
-            following, was feared by the British and is admired by the world,
-            even though he had no money, power, arms or ammunition.
-          </p>
+
         </div>
         <img
           className="img"
-          src="https://i0.wp.com/lawyersgyan.com/wp-content/uploads/2018/05/varna-system-caste-system-in-india.jpg?fit=960%2C540&ssl=1"
+          src="https://i.pinimg.com/564x/71/b3/83/71b3834013f62152400ed677c8a9f5af.jpg"
         />
       </div>
 
